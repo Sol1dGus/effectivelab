9bcd20b2da624198b07e48b749c62b8ff96bcf13
solidgus <coolislankin@mail.ru>
2025-03-25 12:26:21 +0600

    Made navigation for second screen, made button for comeback in home screen, made second screen with right picture for current hero

--- app/src/main/java/HeroCard.kt
+++ app/src/main/java/HeroCard.kt
@@ -1,14 +1,9 @@
-import android.R.attr.color
-import android.R.color
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
--- app/src/main/java/NavGraph.kt
+++ app/src/main/java/NavGraph.kt
@@ -1,6 +1,4 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
